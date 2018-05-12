54bab2ba661943df1eb8755e078c3edba9a57294
Wayne Bagguley <wayne.bagguley@binarysnow.com>
2018-05-12 13:45:24 +0100

    Merging changes

--- src/main/java/com/binarysnow/natty/NatsClient.java
+++ src/main/java/com/binarysnow/natty/NatsClient.java
@@ -6,6 +6,7 @@ import com.binarysnow.natty.io.Initialiser;
 import com.google.common.io.BaseEncoding;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
